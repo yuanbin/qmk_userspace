6ebed71e3cfecd8182000f8bc83311ea84c55991
Yuanbin Shen <ybshen@fortinet.com>
2025-11-06 12:15:36 -0800

    Add smart layer processing and use it for NUMWORD

--- users/yuan/custom_process.c
+++ users/yuan/custom_process.c
@@ -16,8 +16,29 @@ combo_t key_combos[COMBO_LENGTH] = {
 };
 #endif
 
-bool            sw_app_active = false;
-bool            sw_win_active = false;
+static bool sw_app_active = false;
+static bool sw_win_active = false;
+
+static bool num_layer_active = false;
+static const uint16_t num_keys[] = {
+  KC_1,
+  KC_2,
+  KC_3,
+  KC_4,
+  KC_5,
+  KC_6,
+  KC_7,
+  KC_8,
+  KC_9,
+  KC_0,
+  KC_DOT,
+  KC_PLUS,
+  KC_MINUS,
+  KC_ASTR,
+  KC_SLASH,
+  KC_BSPC
+};
+
 static uint16_t sticky_timer  = 0;
 
 void sticky_shift_lt(const uint16_t layer, keyrecord_t *record) {
@@ -72,52 +93,89 @@ bool process_combo_swapper(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// if layer is active, and a key in keys are pressed, keep it active, otherwise, disable the layer.
+void process_smart_layer(const uint16_t layer, bool* layer_active,
+                         const uint16_t* keys, const size_t keys_size,
+                         uint16_t keycode, keyrecord_t *record) {
+  if (*layer_active && record->event.pressed) {
+    switch (keycode) {
+    case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+    case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+    case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+      // Earlier return if this has not been considered tapped yet
+      if (record->tap.count == 0)
+        return;
+      keycode = keycode & 0xFF;
+      break;
+    default:
+      break;
+    }
+    for (size_t i = 0; i < keys_size; i++) {
+      if (keycode == keys[i]) {
+        return;
+      }
+    }
+    // pressed a key not in keys, disable the layer
+    *layer_active = false;
+    layer_off(layer);
+  }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_combo_swapper(keycode, record)) return false;
     /* if (!update_swapper(&sw_app_active, KC_LGUI, KC_TAB, SW_APP, keycode, record)) return false; // has update, stop process */
     /* if (!update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, keycode, record)) return false; // has update, stop process */
-    if (!process_record_num_word(NUMWORD, L_NUMBERS, keycode, record)) return false;
+    /* if (!process_record_num_word(NUMWORD, L_NUMBERS, keycode, record)) return false; */
+    process_smart_layer(L_NUMBERS, &num_layer_active, num_keys, sizeof(num_keys), keycode, record);
 
     switch (keycode) {
-        case ST_MACRO_HOME_DIR:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(MACRO_DELAY) SS_TAP(X_SLASH));
-            }
-            break;
-        case ST_MACRO_PAREN_DIR:
-            if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_DOT) SS_DELAY(MACRO_DELAY) SS_TAP(X_DOT) SS_DELAY(MACRO_DELAY) SS_TAP(X_SLASH));
-            }
-            break;
-        case STICKY_SHIFT_L_NUM:
-            sticky_shift_lt(L_NUM, record);
-            return false; // prevent further processing
-        case STICKY_SHIFT_L_NAV:
-            sticky_shift_lt(L_NAV, record);
-            return false;
+    case ST_MACRO_HOME_DIR:
+      if (record->event.pressed) {
+        SEND_STRING(SS_LSFT(SS_TAP(X_GRAVE)) SS_DELAY(MACRO_DELAY) SS_TAP(X_SLASH));
+      }
+      break;
+    case ST_MACRO_PAREN_DIR:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_DOT) SS_DELAY(MACRO_DELAY) SS_TAP(X_DOT) SS_DELAY(MACRO_DELAY) SS_TAP(X_SLASH));
+      }
+      break;
+    case STICKY_SHIFT_L_NUM:
+      sticky_shift_lt(L_NUM, record);
+      return false; // prevent further processing
+    case STICKY_SHIFT_L_NAV:
+      sticky_shift_lt(L_NAV, record);
+      return false;
     }
     return true;
 }
 
 #if 1
 void process_combo_event(uint16_t combo_index, bool pressed) {
-    switch (combo_index) {
-        case DF:
-            if (pressed) {
-                sw_app_active = true;
-                register_code(KC_LGUI);
-                tap_code(KC_TAB);
-            }
-            break;
-        case JK:
-            if (pressed) {
-                sw_win_active = true;
-                register_code(KC_LALT);
-                tap_code(KC_TAB);
-            }
-            break;
-        default:
-            break;
+  switch (combo_index) {
+  case DF:
+    if (pressed) {
+      sw_app_active = true;
+      register_code(KC_LGUI);
+      tap_code(KC_TAB);
     }
+    break;
+  case JK:
+    if (pressed) {
+      sw_win_active = true;
+      register_code(KC_LALT);
+      tap_code(KC_TAB);
+    }
+    break;
+  case MC:
+    if (pressed) {
+      if (!num_layer_active) {
+        num_layer_active = true;
+        layer_on(L_NUMBERS);
+      }
+    }
+    break;
+  default:
+    break;
+  }
 }
 #endif
