a0df6a526921b29e075b7b8630ffec24c0467438
Yuanbin Shen <yuanbin.shen@gmail.com>
2026-01-23 00:07:31 -0800

    Add keymap for ferris/sweep

--- keyboards/ferris/keymaps/34key-plus/config.h
+++ keyboards/ferris/keymaps/34key-plus/config.h
@@ -34,8 +34,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Pick good defaults for enabling homerow modifiers
 // #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 0
+// #define PERMISSIVE_HOLD
+// #define QUICK_TAP_TERM 0
 
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
